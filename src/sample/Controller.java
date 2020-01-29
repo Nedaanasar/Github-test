05c88ae5710fb0511ff344dbd2646fbcacf233bf
nedaa <Nedaanasar>
2020-01-29 16:22:45 +0100

    First Commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,14 @@
+package sample;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Controller {
+
+    @FXML
+    void trykkMeg(ActionEvent event) {
+        System.out.println("Hello Git");
+    }
+
+}
