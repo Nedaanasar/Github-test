3cd655714cbc04bf55d6f9df86820a03d14474e1
nedaa <Nedaanasar>
2020-01-29 16:26:41 +0100

    Second Commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,10 +5,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class Controller {
+   /** En TEST */
 
     @FXML
     void trykkMeg(ActionEvent event) {
         System.out.println("Hello Git");
+
     }
 
 }
